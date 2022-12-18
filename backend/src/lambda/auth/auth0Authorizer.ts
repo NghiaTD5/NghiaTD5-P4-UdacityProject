f96b9bbcbe7139c8eb7904054eeceed91cda14f1
NghiaTD5 <dainghiait1903@gmail.com>
2022-12-18 12:34:30 +0700

    update

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,18 @@
-import {CustomAuthorizerEvent, CustomAuthorizerResult} from 'aws-lambda'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify } from 'jsonwebtoken'
+import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+
 const logger = createLogger('auth')
-// import { secretsManager } from 'middy/middlewares'
+
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const secretId = process.env.AUTH_0_SECRET_ID
-// const secretField = process.env.AUTH_0_SECRET_FIELD
+const jwksUrl = 'https://dev-kmqb1zim.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,29 +56,20 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const cert = `-----BEGIN CERTIFICATE-----
-MIIDHTCCAgWgAwIBAgIJFFUA37zELAIhMA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
-BAMTIWRldi0xNmJrZ2IwNjZ0NWVicHlzLnVzLmF1dGgwLmNvbTAeFw0yMjEyMTAw
-MjE3NTZaFw0zNjA4MTgwMjE3NTZaMCwxKjAoBgNVBAMTIWRldi0xNmJrZ2IwNjZ0
-NWVicHlzLnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
-ggEBAMbCCle155v2mBUEjbpjt8JOuqELUo29ntmj5qxeUYjIM6VPGf5KzR3QNTu6
-MdpPsHC1FojPCdlBViPKUc+93b94EUnVPNG1pxHzZ+B/b5l2Dvy2nubwuuaAqhn/
-uPlIEjkrdDZUyNrGjD2wBz0bVKxy41ro6ceotsE5cbzkZS1Vl0d7aFGHKry5KWTj
-DJTC+GYHj5Kw8NpEGeQtmlADWkd9xpaunkpAg1sGOAgVCnYHQ2P8EKW9gYaYl8l6
-HfmIXO7Tn2DGh2ALhMlTLS5L10pr2kJOp0+YZsnYIOl9PDDvQjwR/F4X8+qfW6qu
-xS8//tC0Og9vdzSHulwiMfcxoAUCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
-BgNVHQ4EFgQUwBfvWXAXPuvOE6HlgTLy716v9NwwDgYDVR0PAQH/BAQDAgKEMA0G
-CSqGSIb3DQEBCwUAA4IBAQAoJsJbhM7VtqTSZl3e1V1D7cKeq39o4jcRxIokgb1y
-Qut1TaeVgIOh3v2pv8QSv9Lxry3Z2Kc8iJtGTrblHa6U0D3QTF1MKRLMDZVKwxwo
-VMaL/iLeFWU8YX0Y6rUnBaPKmRf9mKNTxmWDEfvgwyNulF3bCICVO8q9apl9lpS/
-Lot/waRCpldQ14eppwIgwqOTpb9ubl0Du/Nojt8GsOCNX+VFd0MswsidNBL8i5pd
-gO6ZZfEFD9pxtZdImmlKNNF+AzIYlkyiRucSw8MRf1yHAogCjRPrgA0vJ0J7SgQo
-XiKOiOVz8NcptsjJen8ah0A03mPdlGho7oUURUHNsXBI
------END CERTIFICATE-----`
-  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  // TODO: Implement token verification done
+  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
+  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+  // return undefined
+  const { header } = jwt;
+
+  let key = await getSigningKey(jwksUrl, header.kid)
+  return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
+
 }
 
-function getToken(authHeader: string): string {
+ function  getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
 
   if (!authHeader.toLowerCase().startsWith('bearer '))
@@ -88,8 +79,38 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
-  // return split[1] as string
-
-  // return token
 }
 
+const getSigningKey = async (jwkurl, kid) => {
+  let res = await Axios.get(jwkurl, {
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Credentials': true,
+    }
+  });
+  let keys  = res.data.keys;
+  // since the keys is an array its possible to have many keys in case of cycling.
+  const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
+      && key.kty === 'RSA' // We are only supporting RSA
+      && key.kid           // The `kid` must be present to be useful for later
+      && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
+    ).map(key => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+    });
+  const signingKey = signingKeys.find(key => key.kid === kid);
+  if(!signingKey){
+    throw new Error('Invalid signing keys')
+    logger.error("No signing keys found")
+  }
+
+  logger.info("Signing keys created successfully ", signingKey)
+  return signingKey
+
+};
+
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
+}
--- client/src/config.ts
+++ client/src/config.ts
@@ -6,6 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-16bkgb066t5ebpys.us.auth0.com',            // Auth0 domain
-  clientId: 'Gz3k1pPJI0NHJi8sQ5ekIItBJw9renQf',          // Auth0 client id
+  clientId: 'ebX6R5HQyK9vnvGBtqEoMuUrRHFDEuHc',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
