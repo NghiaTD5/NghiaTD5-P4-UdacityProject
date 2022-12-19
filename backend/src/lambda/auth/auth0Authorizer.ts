c899b5ae032d5ce2d7d6f56fcab63fcc6e292100
NghiaTD5 <dainghiait1903@gmail.com>
2022-12-19 09:38:25 +0700

    update

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,12 +1,15 @@
-import {CustomAuthorizerEvent, CustomAuthorizerResult} from 'aws-lambda'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify } from 'jsonwebtoken'
+import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
+import * as JwksRsa from 'jwks-rsa';
+import { CertSigningKey } from 'jwks-rsa';
+import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 const logger = createLogger('auth')
+const jwksUrl = process.env.JWKS_ENDPOINT_URL
+const jwksClient = JwksRsa({ jwksUri: jwksUrl })
 // import { secretsManager } from 'middy/middlewares'
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
@@ -56,26 +59,10 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const cert = `-----BEGIN CERTIFICATE-----
-  MIIDHTCCAgWgAwIBAgIJFFUA37zELAIhMA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
-  BAMTIWRldi0xNmJrZ2IwNjZ0NWVicHlzLnVzLmF1dGgwLmNvbTAeFw0yMjEyMTAw
-  MjE3NTZaFw0zNjA4MTgwMjE3NTZaMCwxKjAoBgNVBAMTIWRldi0xNmJrZ2IwNjZ0
-  NWVicHlzLnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
-  ggEBAMbCCle155v2mBUEjbpjt8JOuqELUo29ntmj5qxeUYjIM6VPGf5KzR3QNTu6
-  MdpPsHC1FojPCdlBViPKUc+93b94EUnVPNG1pxHzZ+B/b5l2Dvy2nubwuuaAqhn/
-  uPlIEjkrdDZUyNrGjD2wBz0bVKxy41ro6ceotsE5cbzkZS1Vl0d7aFGHKry5KWTj
-  DJTC+GYHj5Kw8NpEGeQtmlADWkd9xpaunkpAg1sGOAgVCnYHQ2P8EKW9gYaYl8l6
-  HfmIXO7Tn2DGh2ALhMlTLS5L10pr2kJOp0+YZsnYIOl9PDDvQjwR/F4X8+qfW6qu
-  xS8//tC0Og9vdzSHulwiMfcxoAUCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
-  BgNVHQ4EFgQUwBfvWXAXPuvOE6HlgTLy716v9NwwDgYDVR0PAQH/BAQDAgKEMA0G
-  CSqGSIb3DQEBCwUAA4IBAQAoJsJbhM7VtqTSZl3e1V1D7cKeq39o4jcRxIokgb1y
-  Qut1TaeVgIOh3v2pv8QSv9Lxry3Z2Kc8iJtGTrblHa6U0D3QTF1MKRLMDZVKwxwo
-  VMaL/iLeFWU8YX0Y6rUnBaPKmRf9mKNTxmWDEfvgwyNulF3bCICVO8q9apl9lpS/
-  Lot/waRCpldQ14eppwIgwqOTpb9ubl0Du/Nojt8GsOCNX+VFd0MswsidNBL8i5pd
-  gO6ZZfEFD9pxtZdImmlKNNF+AzIYlkyiRucSw8MRf1yHAogCjRPrgA0vJ0J7SgQo
-  XiKOiOVz8NcptsjJen8ah0A03mPdlGho7oUURUHNsXBI
-  -----END CERTIFICATE-----`
-  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  //get cert sign from provided url
+  const cert = await jwksClient.getSigningKey(jwt.header.kid) as CertSigningKey
+  return verify(token, cert.getPublicKey(), { algorithms: ['RS256'] }) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
